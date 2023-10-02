4fcfd0b2f1e70883abe4265b89f981964039bb77
jacksonz16 <jacksonz2390@gmail.com>
2023-10-02 09:26:34 -0400

    1

---
+++ app.js
@@ -0,0 +1,19 @@
+//declare and definefunction input and output machine
+function test(connected){
+    console.log("connected")   
+}
+//run the funaction
+Test("45");
+//string " "
+const x = 420
+//declear variable,define variable
+test(x);
+//
+const y = 69
+test(y)
+const z = function add(x+y);
+
+test
+add(42,2)
+let a =number(prompt(enter a number)
+let b =number(prompt(enter anter number)
